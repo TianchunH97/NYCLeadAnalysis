3b88f1e4bb4d029cb81e6edb2a696776ba5bd35e
tianchunh97 <tianchun@umich.edu>
2020-12-18 04:05:40 +0800

    update

--- docs/scripts/scatter.js
+++ docs/scripts/scatter.js
@@ -1,4 +1,4 @@
-var data = [
+var data1 = [
   [1, 20, 1], [2, 40, 1], [3, 60, 1],
   [4, 80, 2], [5, 100, 2], [6, 120, 2],
   [7, 140, 3], [8, 160, 3], [9, 200, 3]
@@ -24,136 +24,144 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
-// Add X axis
-var xScale = d3.scaleLinear()
-  .domain([0, 20])
-  .range([ 0, width]);
+d3.csv("https://raw.githubusercontent.com/TianchunH97/NYCLeadAnalysis/main/join_2016.csv", function(data) { 
 
-var xAxis = d3.axisBottom().scale(xScale);
+  // Add X axis
+  var xScale = d3.scaleLinear()
+    .domain([0, 0.5])
+    .range([ 0, width]);
 
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(xAxis);
+  var xAxis = d3.axisBottom().scale(xScale);
 
-// Add Y axis
-var yScale = d3.scaleLinear()
-  .domain([0, 200])
-  .range([ height, 0]);
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
-svg.append("g")
-  .call(d3.axisLeft(yScale));
+  // Add Y axis
+  var yScale = d3.scaleLinear()
+    .domain([0, 60])
+    .range([ height, 0]);
 
-// Add X axis label
-svg.append("text")
+  svg.append("g")
+    .call(d3.axisLeft(yScale));
+
+  // Add X axis label
+  svg.append("text")
+    .attr("text-anchor", "end")
+    .attr("x", width)
+    .attr("y", height + margin.top + 20)
+    .text("children lead level");
+
+  // Add Y axis label
+  svg.append("text")
   .attr("text-anchor", "end")
-  .attr("x", width)
-  .attr("y", height + margin.top + 20)
-  .text("children lead level");
+  .attr("transform", "rotate(-90)")
+  .attr("y", -margin.left+20)
+  .attr("x", -margin.top)
+  .text("water lead level")
 
-// Add Y axis label
-svg.append("text")
-.attr("text-anchor", "end")
-.attr("transform", "rotate(-90)")
-.attr("y", -margin.left+20)
-.attr("x", -margin.top)
-.text("water lead level")
+  // Add tooltip
+  var tooltip = d3.select("#my_dataviz")
+  .append("div")
+  .style("opacity", 0)
+  .attr("class", "tooltip")
+  .style("background-color", "white")
+  .style("border", "solid")
+  .style("border-width", "2px")
+  .style("border-radius", "5px")
+  .style("padding", "5px")
+  .style("position", "absolute")
 
-// Add tooltip
-var tooltip = d3.select("#my_dataviz")
-.append("div")
-.style("opacity", 0)
-.attr("class", "tooltip")
-.style("background-color", "white")
-.style("border", "solid")
-.style("border-width", "2px")
-.style("border-radius", "5px")
-.style("padding", "5px")
-.style("position", "absolute")
-
-var mouseover = function(d) {
-  tooltip
-    .style("opacity", 1);
-}
-
-var mousemove = function(event, d) {
-  tooltip
-    .html("data: (" + d[0] + ", " + d[1] + "), borough: " + d[2])
-    .style("left", (d3.pointer(event)[0]+60) + "px")
-    .style("top", (d3.pointer(event)[1]+150) + "px");
-}
-
-var mouseleave = function(d) {
-  tooltip
-    .transition()
-    .duration(1)
-    .style("opacity", 0);
-}
-
-// Color scale
-var color = d3.scaleOrdinal()
-.domain([1, 2, 3])
-.range([ "#F8766D", "#00BA38", "#619CFF"])
-
-// Add dots
-svg.append('g')
-  .selectAll("circle")
-  .data(data)
-  .enter()
-  .append("circle")
-    .attr("cx", function(d) {return xScale(d[0]);})
-    .attr("cy", function(d) {return yScale(d[1]);})
-    .attr("r", 5)
-    .style("fill", function(d) {return color(d[2]);})
-  .on("mouseover", mouseover )
-  .on("mousemove", mousemove )
-  .on("mouseleave", mouseleave )
-
-// Interactive components
-d3.selectAll("p")
-  .on("click", function () {
-
-  var paraID = d3.select(this).attr("id");
-
-  if (paraID == "change_data") {
-    svg.selectAll("circle")
-      .data(data2)
-      .transition()
-      .duration(2000)
-        .attr("cx", function(d) {return xScale(d[0]);})
-        .attr("cy", function(d) {return yScale(d[1]);})
-        .attr("r", 5)
-        .style("fill", function(d) {return color(d[2]);})
-  } else {
-    svg.selectAll("circle")
-      .data(data)
-      .transition()
-      .duration(2000)
-        .attr("cx", function(d) {return xScale(d[0]);})
-        .attr("cy", function(d) {return yScale(d[1]);})
-        .attr("r", 5)
-        .style("fill", function(d) {return color(d[2]);})
+  var mouseover = function(d) {
+    tooltip
+      .style("opacity", 1);
   }
+
+  var mousemove = function(event, d) {
+    tooltip
+      .html("data: (" + d[2] + ", " + d[3] + "), borough: " + d[1])
+      .style("left", (d3.pointer(event)[0]+60) + "px")
+      .style("top", (d3.pointer(event)[1]+150) + "px");
+  }
+
+  var mouseleave = function(d) {
+    tooltip
+      .transition()
+      .duration(1)
+      .style("opacity", 0);
+  }
+
+  // Color scale
+  var color = d3.scaleOrdinal()
+  .domain([1, 2, 3])
+  .range([ "#F8766D", "#00BA38", "#619CFF"])
+
+  var borough_map = d3.scaleOrdinal()
+    .domain([1, 2, 3, 4, 5])
+    .range(["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten Island"])
+
+  // Add dots
+  svg.append('g')
+    .selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function(d) {return xScale(d[3]);})
+      .attr("cy", function(d) {return yScale(d[2]);})
+      .attr("r", 5)
+      .style("fill", function(d) {return color(d[0]);})
+    .on("mouseover", mouseover )
+    .on("mousemove", mousemove )
+    .on("mouseleave", mouseleave )
+
+  // Interactive components
+  d3.selectAll("p")
+    .on("click", function () {
+
+    var paraID = d3.select(this).attr("id");
+
+    if (paraID == "change_data") {
+      svg.selectAll("circle")
+        .data(data2)
+        .transition()
+        .duration(2000)
+          .attr("cx", function(d) {return xScale(d[0]);})
+          .attr("cy", function(d) {return yScale(d[1]);})
+          .attr("r", 5)
+          .style("fill", function(d) {return color(d[2]);})
+    } else {
+      svg.selectAll("circle")
+        .data(data)
+        .transition()
+        .duration(2000)
+          .attr("cx", function(d) {return xScale(d[3]);})
+          .attr("cy", function(d) {return yScale(d[2]);})
+          .attr("r", 5)
+          .style("fill", function(d) {return color(d[0]);})
+    }
+  });
+
+  // Add legends
+  var size = 20
+  svg.selectAll("mydots")
+    .data([1,2,3])
+    .enter()
+    .append("rect")
+      .attr("x", 360)
+      .attr("y", function(d,i){ return 20 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+      .attr("width", size)
+      .attr("height", size)
+      .style("fill", function(d){ return color(d)})
+
+  svg.selectAll("mylabels")
+    .data([1,2,3])
+    .enter()
+    .append("text")
+      .attr("x", 360 + size*1.2)
+      .attr("y", function(d,i){ return 20 + i*(size+5) + (size/2)}) 
+      .style("fill", function(d){ return color(d)})
+      .text(function(d){ return d})
+      .attr("text-anchor", "left")
+      .style("alignment-baseline", "middle")
+
 });
-
-// Add legends
-var size = 20
-svg.selectAll("mydots")
-  .data([1,2,3])
-  .enter()
-  .append("rect")
-    .attr("x", 360)
-    .attr("y", function(d,i){ return 20 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
-    .attr("width", size)
-    .attr("height", size)
-    .style("fill", function(d){ return color(d)})
-
-svg.selectAll("mylabels")
-  .data([1,2,3])
-  .enter()
-  .append("text")
-    .attr("x", 360 + size*1.2)
-    .attr("y", function(d,i){ return 20 + i*(size+5) + (size/2)}) 
-    .style("fill", function(d){ return color(d)})
-    .text(function(d){ return d})
-    .attr("text-anchor", "left")
-    .style("alignment-baseline", "middle")
